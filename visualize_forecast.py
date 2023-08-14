a178c9a0bd343ea082a8d11cf7e65aeb58950232
Thomas Gessey-Jones <tg400@cam.ac.uk>
2023-08-14 13:25:54 +0100

    Rotate ticks

--- visualize_forecast.py
+++ visualize_forecast.py
@@ -365,6 +365,10 @@ def detectability_corner_plot(
     cbar.set_ticklabels([rf"{tick * 100:.0f}\%" for tick in
                          cbar.get_ticks()])
 
+    # Rotate x tick labels for bottom row, to avoid overlap
+    for ax in axes[-1, :]:
+        ax.tick_params(axis='x', labelrotation=90)
+
     # Return the figure handle
     return fig
 
