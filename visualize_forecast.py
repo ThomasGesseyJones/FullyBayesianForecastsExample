bf1f26b8ed1084add5fbf42a72e57cb458446494
Thomas Gessey-Jones <tg400@cam.ac.uk>
2023-08-14 13:10:57 +0100

    Formatting

--- visualize_forecast.py
+++ visualize_forecast.py
@@ -393,13 +393,14 @@ def main():
 
     # Set-up plotting style and variables
     plt.style.use(os.path.join('figures', 'mnras_single.mplstyle'))
+    plt.rcParams.update({'figure.figsize': (3.15, 3.15)})
     plt.rcParams.update({'ytick.labelsize': 5})
     plt.rcParams.update({'xtick.labelsize': 5})
     plt.rcParams.update({'axes.labelsize': 5})
     plt.rcParams.update({'figure.titlesize': 8})
     plt.rcParams.update({'figure.subplot.bottom': 0.08})
-    plt.rcParams.update({'figure.subplot.right': 0.96})
-    plt.rcParams.update({'figure.subplot.top': 0.96})
+    plt.rcParams.update({'figure.subplot.right': 0.92})
+    plt.rcParams.update({'figure.subplot.top': 0.92})
     plt.rcParams.update({'figure.subplot.left': 0.14})
 
     detection_thresholds = config_dict["detection_thresholds"]
