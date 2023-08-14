a34520ce65bcda90aa9ced7f63a5e73106276fd6
Thomas Gessey-Jones <tg400@cam.ac.uk>
2023-08-14 16:57:46 +0100

    Code to run polychord

--- verification_with_polychord.py
+++ verification_with_polychord.py
@@ -17,10 +17,262 @@ noise sigma in K. The default is 0.025 K. This indicates the evidence
 network to use for the comparison.
 """
 
+# Required imports
+from __future__ import annotations
+from typing import Callable, Tuple
+from train_evidence_network import get_noise_sigma, load_configuration_dict
+from simulators.twenty_one_cm import load_globalemu_emulator, \
+    global_signal_experiment_measurement_redshifts, GLOBALEMU_INPUTS, \
+    GLOBALEMU_PARAMETER_RANGES
+import os
+import shutil
+import numpy as np
+from pypolychord import PolyChordSettings, run_polychord
+from pypolychord.priors import UniformPrior, GaussianPrior, LogUniformPrior
+from copy import deepcopy
+from scipy.stats import truncnorm
+
+# Parameters
+CHAIN_DIR = "chains"
+
+
+# Prior, likelihood, and evidence functions
+def noise_only_log_evidence(data: np.ndarray, sigma_noise: float) -> float:
+    """Evaluate the log evidence for a noise only model.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        The mock data to evaluate the log evidence for.
+    sigma_noise : float
+        The noise sigma in K.
+
+    Returns
+    -------
+    log_evidence : float
+        The log evidence for the noise only model.
+    """
+    num_data_points = data.size
+    log_evidence = -0.5*num_data_points*np.log(2*np.pi*sigma_noise**2) \
+        - 0.5 * np.sum(data**2) / sigma_noise**2
+    return log_evidence
+
+
+def generate_noisy_signal_loglikelihood(data: np.ndarray,
+                                        sigma_noise: float,
+                                        globalemu_emulator: Callable
+                                        ) -> Callable:
+    """Generate a loglikelihood function for a noisy signal model.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        The mock data to evaluate the loglikelihood for.
+    sigma_noise : float
+        The noise sigma in K.
+    globalemu_emulator : Callable
+        The emulator for the global signal.
+
+    Returns
+    -------
+    loglikelihood : Callable
+        The loglikelihood function for the noisy signal model.
+    """
+    def loglikelihood(theta: np.ndarray) -> Tuple[float, list]:
+        """Evaluate the loglikelihood for a noisy signal model."""
+        global_signal_mk, _ = globalemu_emulator(theta)
+        global_signal_k = global_signal_mk / 1000
+        num_data_points = data.size
+        log_evidence = -0.5*num_data_points*np.log(2*np.pi*sigma_noise**2) \
+                       - 0.5*np.sum((data-global_signal_k)**2)/sigma_noise**2
+        return log_evidence, []
+    return loglikelihood
+
+
+class TruncatedGaussianPrior:
+    """Truncated Gaussian prior.
+
+    This prior is a Gaussian distribution with mean mu and standard deviation
+    sigma truncated to the range [low, high].
+    """
+
+    def __init__(self, mu, sigma, low, high):
+        """Initialize the truncated Gaussian prior.
+
+        Parameters
+        ----------
+        mu : float
+            The mean of the would have been Gaussian prior.
+        sigma : float
+            The standard deviation of the would have been Gaussian prior.
+        low : float
+            The lower bound of the truncated Gaussian prior.
+        high : float
+            The upper bound of the truncated Gaussian prior.
+        """
+        self.mu = mu
+        self.sigma = sigma
+        self.low = low
+        self.high = high
+
+        # Define utility variables
+        self.alpha = (self.low - self.mu) / self.sigma
+        self.beta = (self.high - self.mu) / self.sigma
+
+    def __call__(self, x: np.ndarray | float) -> np.ndarray | float:
+        """Sample the prior.
+
+        Parameters
+        ----------
+        x : np.ndarray or float
+            Sample from a unit hypercube.
+
+        Returns
+        -------
+        prior : np.ndarray or float
+            The equivalent sampled parameter value(s) in the original parameter
+            space.
+        """
+        scaled_values = truncnorm.ppf(x, self.alpha, self.beta)
+        return self.mu + self.sigma * scaled_values
+
+
+def generate_prior(config_dict: dict) -> Callable:
+    """Generate a prior function for the global signal model.
+
+    Parameters
+    ----------
+    config_dict : dict
+        The configuration dictionary for the pipeline.
+
+    Returns
+    -------
+    prior : Callable
+        The prior callable for the global signal model.
+    """
+    # Loop over parameters constructing individual prior objects
+    individual_priors = []
+    for param in GLOBALEMU_INPUTS:
+        # Get prior info
+        prior_info = deepcopy(config_dict['priors'][param])
+
+        # Replace emu_min and emu_max with the min and max value globalemu
+        # can take for this parameter
+        for k, v in prior_info.items():
+            if v == 'emu_min':
+                prior_info[k] = GLOBALEMU_PARAMETER_RANGES[param][0]
+            elif v == 'emu_max':
+                prior_info[k] = GLOBALEMU_PARAMETER_RANGES[param][1]
+
+        # Get prior type
+        prior_type = prior_info.pop('type')
+        if prior_type == 'uniform':
+            param_prior = UniformPrior(prior_info['low'], prior_info['high'])
+        elif prior_type == 'log_uniform':
+            param_prior = LogUniformPrior(prior_info['low'],
+                                          prior_info['high'])
+        elif prior_type == 'gaussian':
+            param_prior = GaussianPrior(prior_info['mean'], prior_info['std'])
+        elif prior_type == 'truncated_gaussian':
+            param_prior = TruncatedGaussianPrior(prior_info['mean'],
+                                                 prior_info['std'],
+                                                 prior_info['low'],
+                                                 prior_info['high'])
+        else:
+            raise ValueError("Unknown prior type.")
+
+        # Add to list of priors
+        individual_priors.append(param_prior)
+
+    # Define prior function
+    def prior(x: np.ndarray) -> np.ndarray:
+        """Sample the prior.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Sample from a unit hypercube.
+
+        Returns
+        -------
+        theta : np.ndarray
+            The equivalent sampled parameter values in the original parameter
+            space.
+        """
+        theta = np.zeros_like(x)
+        for idx, sample_transform in enumerate(individual_priors):
+            theta[idx] = sample_transform(x[idx])
+        return theta
+    return prior
+
 
 def main():
     """Verify accuracy of evidence network with Polychord."""
-    pass
+    # Get noise sigma and configuration data
+    sigma_noise = get_noise_sigma()
+    config_dict = load_configuration_dict()
+
+    # Load verification data
+    verification_data_file = (
+        os.path.join('verification_data',
+                     f'noise_{sigma_noise:.4f}_verification_data.npz'))
+    verification_data_file_contents = np.load(verification_data_file)
+    v_data = verification_data_file_contents['data']
+    en_log_bayes_ratios = verification_data_file_contents['log_bayes_ratios']
+
+    # Set up global emu
+    globalemu_redshifts = global_signal_experiment_measurement_redshifts(
+        config_dict['frequency_resolution'])
+    globalemu_predictor = load_globalemu_emulator(globalemu_redshifts)
+
+    # Generate priors
+    prior = generate_prior(config_dict)
+
+    # Make sure chains directory exists
+    os.makedirs(CHAIN_DIR, exist_ok=True)
+
+    # Loop over data using Polychord to evaluate the evidence
+    pc_log_bayes_ratios = []
+    for data in v_data:
+        # Can find noise only evidence analytically
+        log_z_noise_only = noise_only_log_evidence(data, sigma_noise)
+
+        # Use Polychord to find evidence for noise + global signal
+        loglikelihood = generate_noisy_signal_loglikelihood(
+            data, sigma_noise, globalemu_predictor
+        )
+
+        # Set Polychord properties
+        n_dims = len(GLOBALEMU_INPUTS)
+        n_derived = 0
+        settings = PolyChordSettings(n_dims, n_derived)
+        settings.nlive = 25 * n_dims  # As recommended
+        settings.base_dir = os.path.join(CHAIN_DIR, f'noise_{sigma_noise:.4f}')
+        settings.file_root = f'noise_{sigma_noise:.4f}'
+        settings.do_clustering = True
+        settings.read_resume = False
+
+        # Clear out base directory ready for the run
+        try:
+            shutil.rmtree(settings.base_dir)
+        except OSError:
+            pass
+        try:
+            os.mkdir(settings.base_dir)
+        except OSError:
+            pass
+
+        # Run polychord
+        output = run_polychord(loglikelihood, n_dims,
+                               n_derived, settings, prior)
+        log_z_noisy_signal = output.logZ
+
+        # Compute log bayes ratio
+        log_bayes_ratio = log_z_noisy_signal - log_z_noise_only
+        pc_log_bayes_ratios.append(log_bayes_ratio)
+
+    print(en_log_bayes_ratios)
+    print(pc_log_bayes_ratios)
 
 
 if __name__ == "__main__":
